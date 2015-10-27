ee5eabf5943b0913798c3ed754cd9e36364f0372
Shinichiro Suzuki <shinichiro.su@gmail.com>
2015-10-28 07:34:56 +0900

    型名を変更（JSONKifuFormat -> JKF）して、あとexportしている型をToJSON、 FromJSONのinstanceにした

--- src/JKF/Type.hs
+++ src/JKF/Type.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 module JKF.Type (
-    JSONKifuFormat(..)
+    JKF(..)
   , Header
   , Initial(..)
   , StateFormat(..)
@@ -13,28 +14,51 @@ module JKF.Type (
   , PlaceFormat(..)
   ) where
 
+import           Data.Aeson     ((.:), (.=))
+import           Data.Aeson     as A
 import           Data.Set       (Set)
 import           Data.Text.Lazy (Text)
-import           GHC.Generics
+import           GHC.Generics   (Generic)
 
-data JSONKifuFormat = JSONKifuFormat {
-    jsonKifuFormatHeader  :: [Header]
-  , jsonKifuFormatInitial :: Maybe Initial
-  , jsonKifuFormatMoves   :: [MoveFormat]
+data JKF = JKF {
+    jkfHeader  :: Header
+  , jkfInitial :: Maybe Initial
+  , jkfMoves   :: [MoveFormat]
   } deriving (Show, Eq, Generic)
 
-type Header = (Text, Text)
+instance A.ToJSON JKF where
+instance A.FromJSON JKF where
+
+data Header = Header {
+    headerBlack          :: String
+  , headerWhite          :: String
+  , headerStartDatetime  :: String
+  , headerFinishDatetime :: String
+  , headerPresetString   :: String
+  } deriving (Show, Eq, Generic)
+
+instance A.ToJSON Header where
+  toJSON (Header b w s f p) = object ["開始日時" .= s, "終了日時" .= f, "手合割" .= p, "先手" .= b, "後手" .= w]
+
+instance A.FromJSON Header where
+  parseJSON (A.Object v) = Header <$> v .: "開始日時" <*> v .: "終了日時" <*> v .: "手合割" <*> v .: "先手" <*> v .: "後手"
 
 data Initial = Initial {
     initialPreset :: Text
   , initialData   :: StateFormat
   } deriving (Show, Eq, Generic)
 
+instance A.ToJSON Initial where
+instance A.FromJSON Initial where
+
 data StateFormat = StateFormat {
     stateFormatColor :: Color
   , stateFormatBoard :: Board
   , stateFormatHands :: [Hand]
   } deriving (Show, Eq, Generic)
+
+instance A.ToJSON StateFormat where
+instance A.FromJSON StateFormat where
 
 data MoveMoveFormat = MoveMoveFormat {
     moveMoveFormatColor    :: Color
@@ -47,6 +71,9 @@ data MoveMoveFormat = MoveMoveFormat {
   , moveMoveFormatRelative :: Maybe String
   } deriving (Show, Eq, Generic)
 
+instance A.ToJSON MoveMoveFormat where
+instance A.FromJSON MoveMoveFormat where
+
 data MoveFormat = MoveFormat {
     moveFormatComments :: Maybe [Text]
   , moveFormatMove     :: Maybe MoveMoveFormat
@@ -55,10 +82,16 @@ data MoveFormat = MoveFormat {
   , moveFormatForks    :: Maybe [[MoveFormat]]
   } deriving (Show, Eq, Generic)
 
+instance A.ToJSON MoveFormat where
+instance A.FromJSON MoveFormat where
+
 data Time = Time {
     timeNow   :: TimeFormat
   , timeTotal :: TimeFormat
   } deriving (Show, Eq, Generic)
+
+instance A.ToJSON Time where
+instance A.FromJSON Time where
 
 data TimeFormat = TimeFormat {
     timeFormatH :: Maybe Int
@@ -66,12 +99,21 @@ data TimeFormat = TimeFormat {
   , timeFormatS :: Int
   } deriving (Show, Eq, Generic)
 
+instance A.ToJSON TimeFormat where
+instance A.FromJSON TimeFormat where
+
 data PlaceFormat = PlaceFormat {
     placeFormatX :: Int
   , placeFormatY :: Int
   } deriving (Show, Eq, Generic)
 
+instance A.ToJSON PlaceFormat where
+instance A.FromJSON PlaceFormat where
+
 data Color = Black | White deriving (Show, Eq, Ord, Generic)
+
+instance A.ToJSON Color where
+instance A.FromJSON Color where
 
 type Board = [[Maybe Cel]]
 
@@ -80,8 +122,14 @@ data Cel = Cel {
   , celKind  :: Piece
 } deriving (Show, Eq, Generic)
 
+instance A.ToJSON Cel where
+instance A.FromJSON Cel where
+
 data Piece = Fu | Ky | Ke | Gi | Ki | Ka | Hi | Ou
            | To | NKy | NKe | NGi | Um | Ry
            deriving (Show, Eq, Ord, Generic)
+
+instance A.ToJSON Piece where
+instance A.FromJSON Piece where
 
 type Hand = Set (Piece, Int)
